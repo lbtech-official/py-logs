abe120e2c90287cb486cec866eed452517eb6191
Alexey Chernyavskii <che.alexey@gmail.com>
2020-08-22 23:02:10 +0300

    Different handlers

--- example.py
+++ example.py
@@ -1,91 +1,52 @@
 """
-Step #4: redirect stdout/stderr to a single file.
+Step #5: different handlers.
 """
 import logging
-# import sys
-# import time
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(process)d-%(levelname)s-%(message)s',
-    # handlers=[logging.StreamHandler(stream=sys.stdout)]  # force use STDOUT instead of STDERR
-)
+# Custom logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)  # remember to configure top-level logging level
+
+# Configure handlers
+# Stream
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.DEBUG)
+stream_formatter = logging.Formatter('%(process)d - %(levelname)s - %(message)s')
+stream_handler.setFormatter(stream_formatter)
+# File
+file_handler = logging.FileHandler(filename='example.log')
+file_handler.setLevel(logging.ERROR)
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+
+# Configure logger with two handlers
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
 
 print('Print')  # Print uses sys.stdout by default
 
-logging.debug('Debug')
-logging.info('Info')
-logging.warning('Warning')
-logging.error('Error')
-logging.critical('Critical')
+# Using configured logger instance
+logger.debug('Debug')
+logger.info('Info')
+logger.warning('Warning')
+logger.error('Error')
+logger.critical('Critical')
 
-# time.sleep(60)  # for Linux demo to show Standart Streams (descriptors) in action
 
+# clear && :> example.log && python3 example.py && cat example.log
 
-# Terminal commands (Unix/Linux):
+# Console Output:
+# Print
+# 34360 - DEBUG - Debug
+# 34360 - INFO - Info
+# 34360 - WARNING - Warning
+# 34360 - ERROR - Error
+# 34360 - CRITICAL - Critical
 
-# === ATTEMPT #1: ===
-# By default, functions debug(), info(), warning(), error() and critical()
-# are set to a destination of the console (sys.stderr)
-# https://docs.python.org/3/howto/logging.html#advanced-logging-tutorial
-
-#   clear && :> example.log && python3 example.py > example.log && cat example.log
-
-#   Console Output:
-
-#   18468-DEBUG-Debug
-#   18468-INFO-Info
-#   18468-WARNING-Warning
-#   18468-ERROR-Error
-#   18468-CRITICAL-Critical
-
-#   File output:
-#   ───────┬───────────────────────
-#          │ File: example.log
-#   ───────┼───────────────────────
-#      1   │ Print
-#   ───────┴───────────────────────
-
-# === ATTEMPT #2: ===
-# Redirecting stdout and stderr using file descriptors.
-# We also can use `>>` redirect as `filemode='a'` analog.
-# Note that `Print` is beyond of log output (incorrect order).
-
-#   clear && :> example.log && python3 example.py &> example.log && cat example.log
-
-#   Console Output:
-#   <empty>
-
-#   File output:
-#   ───────┬───────────────────────────
-#          │ File: example.log
-#   ───────┼───────────────────────────
-#      1   │ 28457-DEBUG-Debug
-#      2   │ 28457-INFO-Info
-#      3   │ 28457-WARNING-Warning
-#      4   │ 28457-ERROR-Error
-#      5   │ 28457-CRITICAL-Critical
-#      6   │ Print
-#   ───────┴───────────────────────────
-
-# === ATTEMPT #3: ===
-# Using `handlers=[logging.StreamHandler(stream=sys.stdout)]` to log directly to
-# stdout to resolve ordering problem.
-# Note that `Print` and log output are displayed in the correct order now.
-
-#   clear && :> example.log && python3 example.py &> example.log && cat example.log
-
-#   Console Output:
-#   <empty>
-
-#   File output:
-#   ───────┬───────────────────────────
-#          │ File: example.log
-#   ───────┼───────────────────────────
-#      1   │ Print
-#      2   │ 28457-INFO-Info
-#      3   │ 28457-WARNING-Warning
-#      4   │ 28457-ERROR-Error
-#      5   │ 28457-CRITICAL-Critical
-#      6   │ 28457-DEBUG-Debug
-#   ───────┴───────────────────────────
+# File output:
+# ───────┬─────────────────────────────────────────────────────────────
+#        │ File: example.log
+# ───────┼─────────────────────────────────────────────────────────────
+#    1   │ 2020-08-22 22:34:45,802 - __main__ - ERROR - Error
+#    2   │ 2020-08-22 22:34:45,802 - __main__ - CRITICAL - Critical
+# ───────┴─────────────────────────────────────────────────────────────
