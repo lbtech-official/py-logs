c2066a4ea2c01f454853c6d504afde9b236b4a7a
Alexey Chernyavskii <che.alexey@gmail.com>
2020-08-23 12:46:12 +0300

    Error handling

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 """
-Step #5: different handlers.
+Step #6: error handling.
 """
+from math import sqrt
 import logging
 
 # Custom logger
@@ -29,24 +30,65 @@ print('Print')  # Print uses sys.stdout by default
 logger.debug('Debug')
 logger.info('Info')
 logger.warning('Warning')
-logger.error('Error')
-logger.critical('Critical')
+
+
+# === Handled exception #1 ===
+try:
+    1/0
+except Exception:
+    logger.error('Handled exception #1', exc_info=True)
+    print('=' * 20)
+
+
+# === Handled exception #2 ===
+try:
+    1/0
+except Exception:
+    logger.exception('Handled exception #2')
+    print('=' * 20)
+
+
+# === Unhandled exceptin #3 ===
+sqrt(-1)
 
 
 # clear && :> example.log && python3 example.py && cat example.log
 
-# Console Output:
+# Console output:
+
 # Print
-# 34360 - DEBUG - Debug
-# 34360 - INFO - Info
-# 34360 - WARNING - Warning
-# 34360 - ERROR - Error
-# 34360 - CRITICAL - Critical
+# 2156 - DEBUG - Debug
+# 2156 - INFO - Info
+# 2156 - WARNING - Warning
+# 2156 - ERROR - Handled exception #1
+# Traceback (most recent call last):
+#   File "example.py", line 37, in <module>
+#     1/0
+# ZeroDivisionError: division by zero
+# ====================
+# 2156 - ERROR - Handled exception #2
+# Traceback (most recent call last):
+#   File "example.py", line 45, in <module>
+#     1/0
+# ZeroDivisionError: division by zero
+# ====================
+# Traceback (most recent call last):
+#   File "example.py", line 52, in <module>
+#     sqrt(-1)
+# ValueError: math domain error
 
 # File output:
-# ───────┬─────────────────────────────────────────────────────────────
+# ───────┬──────────────────────────────────────────────────────────────────────
 #        │ File: example.log
-# ───────┼─────────────────────────────────────────────────────────────
-#    1   │ 2020-08-22 22:34:45,802 - __main__ - ERROR - Error
-#    2   │ 2020-08-22 22:34:45,802 - __main__ - CRITICAL - Critical
-# ───────┴─────────────────────────────────────────────────────────────
+# ───────┼──────────────────────────────────────────────────────────────────────
+#    1   │ 2020-08-23 12:37:25,136 - __main__ - ERROR - Handled exception #1
+#    2   │ Traceback (most recent call last):
+#    3   │   File "example.py", line 37, in <module>
+#    4   │     1/0
+#    5   │ ZeroDivisionError: division by zero
+#    6   │ 2020-08-23 12:37:25,136 - __main__ - ERROR - Handled exception #2
+#    7   │ Traceback (most recent call last):
+#    8   │   File "example.py", line 45, in <module>
+#    9   │     1/0
+#   10   │ ZeroDivisionError: division by zero
+# ───────┴──────────────────────────────────────────────────────────────────────
